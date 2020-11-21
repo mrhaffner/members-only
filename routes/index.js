6c649a4ddc0e2f63bb2c6299dc69c8329640e102
Matt Haffner <mrhaffner13@gmail.com>
2020-11-21 00:48:48 -0600

    added a router skeleton

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', );
+
+router.get('/login', );
+
+router.post('/login', );
+
+router.get('/signup', );
+
+router.post('/signup', );
+
+router.get('/membership', );
+
+router.post('/membership', );
+
+router.get('/new', );
+
+router.post('/new', );
 
 module.exports = router;
